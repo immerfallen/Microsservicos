d1a0a4fc44f6517ee203dc3c6ce9ecf1422b7c0a
immerfallen <marodemelo10@gmail.com>
2024-01-12 15:37:53 -0300

    Criação de values objects

---
+++ BookingService/Adapters/Data/GuestConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal class GuestConfiguration : IEntityTypeConfiguration<Guest>
+    {
+        public void Configure(EntityTypeBuilder<Guest> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.OwnsOne(x => x.Document)
+                .Property(x => x.Idnumber);
+            builder.OwnsOne(x => x.Document)
+                .Property(x => x.DocumentType);
+        }
+    }
+}
